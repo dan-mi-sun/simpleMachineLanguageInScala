7549bf4e204d6a4faee424928a237c14c2edc46a
dan-mi-sun <dan.mi.sun@gmail.com>
2015-02-06 14:45:30 +0000

    add skeleton test for sml subclasses

---
+++ SML_Scala/src/sml/sml_tests.scala
@@ -0,0 +1,11 @@
+import org.scalatest._
+
+class SmlSpecs extends FlatSpec with Matchers {
+//instantiate class object here
+
+//it "should divide by 2" in {
+    val answer = thing.div(4,2)
+    response should be 2
+  }
+  
+}
